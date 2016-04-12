cdae410653a0c0bf2aa9fca3d53e6c2e82713b0a
lealivran <lealivran@msn.com>
2016-04-12 12:15:37 +0200

    add restau list + view + map

--- models/restaurants.js
+++ models/restaurants.js
@@ -1,7 +1,23 @@
 const mongoose = require('mongoose');
+var mongoosePaginate = require('mongoose-paginate');
 
 const RestaurantsSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  cuisine: { type: String, required: true },
+  borough: { type: String, required: true },
+  grades: [{
+    date: { type : Date, default : Date.now },
+    grade: { type : String, default : '' },
+    score: { type : Number, default : 0 },
+  }],
+  address: [{
+    building: { type : String, default : '' },
+    street: { type : String, default : '' },
+    zipcode: { type : String, default : '' },
+  }],
+  restaurant_id: { type: String, required: true },
 });
+
+RestaurantsSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('Restaurant', RestaurantsSchema);
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -3,14 +3,20 @@ var router = express.Router();
 var Restaurant = require('../models/restaurants');
 
 /* GET restaurants listing. */
-router.get('/', function(req, res, next) {
-
-  Restaurant.find(function (err, restaurants) {
+router.get('/:page', function(req, res, next) {
+  Restaurant.paginate({}, {page:req.params.page, limit:50}, function (err, restaurants) {
     if (err) return handleError(err);
-    console.log('%s', restaurants)
+    // console.log('%s', restaurants)
+    res.render('restaurants/index', {title: 'Tous les restaurants', restaurants});
   })
+});
 
-  res.render('restaurants/index', {title: 'Restaurants'});
+router.get('/view/:id', function(req, res, next) {
+  Restaurant.find({restaurant_id:req.params.id}, function (err, restaurant){
+    if (err) console.log(err);
+    // console.log('%s', restaurant)
+    res.render('restaurants/view', {title: 'Restaurant', restaurant});
+  })
 });
 
 module.exports = router;
